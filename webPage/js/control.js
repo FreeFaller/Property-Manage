b6b583e75e36ee2d1eea53d9e13af1b6436593f4
FreeFaller <1398946971@qq.com>
2016-11-24 15:20:26 +0800

    debug

---
+++ webPage/js/control.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    function accordion(trigger) {
+        var $button = $(trigger),
+            visible = true;
+            $button.hover().css({'cursor': 'pointer'});
+        //event
+        $button.click(function() {
+            if ( ! visible ) {
+                $(this).next().slideUp('slow',function() {
+                    $(this).css("display","none")
+                    $('.panel-content').attr( 'aria-expanded','false' );
+                });
+            }else {
+                $(this).next().slideUp(0,function() {
+                    $('.panel-content').attr( 'aria-expanded','true' );
+                    $(this).css("display","block")
+                });
+            }
+            visible = !visible;
+            return false
+        });
+    }
+
+    accordion('#trigger1');
+    accordion('#trigger2');
+    accordion('#trigger3');
+    accordion('#trigger4');
+});
