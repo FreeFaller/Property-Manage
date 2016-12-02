697d215de15f4499d92a0a5fda7a90d204bb2a57
FreeFaller <1398946971@qq.com>
2016-12-02 20:32:01 +0800

    物业清单

--- webPage/js/bill.js
+++ webPage/js/bill.js
@@ -5,12 +5,13 @@ function chooseAll(){
 	if (chooseAllBtn.checked == false) {
 		for (var i = 0; i < checkbox.length; i++) {
 			checkbox[i].checked = false;
-		};
+		}
 	}else{
 		for (var j = 0; j < checkbox.length; j++) {
 			checkbox[j].checked = true;
-		};
+		}
 	}
+	calculate();
 }
 function getElementsByClassName(className, parent) {
     var oParent = parent ? document.getElementById("parent") : document;
@@ -19,13 +20,18 @@ function getElementsByClassName(className, parent) {
         return e.classList.contains(className);
     });
 }
-// function calculate(){
-// 	var parent = document.getElementById("bill");
-// 	var checkbox = document.getElementsByClassName('checkbox',parent);
-// 	var sum = 0;
-// 	for (var i = 0; i < checkbox.length; i++) {
-// 		if(checkbox[i].checked = true){
-			
-// 		}
-// 	};
-// }
+function calculate(){
+	var parent = document.getElementById("bill");
+	var checkbox = document.getElementsByClassName('checkbox',parent);
+	var sum = 0;
+	var totleCost = document.getElementById("totleCost");
+	for (var h = 0; h < checkbox.length; h++) {
+		if(checkbox[h].checked == true){
+			var li = checkbox[h].parentNode;
+			var cost = parseFloat(li.childNodes[5].childNodes[1].childNodes[2].childNodes[1].innerText);
+			sum += cost;
+		}
+	}
+	var finalCost = sum.toFixed(1);
+	totleCost.innerText = finalCost;
+}
